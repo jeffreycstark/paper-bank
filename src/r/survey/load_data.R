516867b2604a741e443d827d879a671652468931
Jeffrey Stark <jeffrey.stark@gmail.com>
2026-02-15 08:05:54 +0700

    Remove duplicated harmonization engine, codebook tools, and pipeline code
    Delete src/r/ (harmonize engine, codebook tools, utils, survey, data_prep_modules),
    src/config/ (YAML specs), src/python/, src/scripts/, claudedocs/, notebooks/,
    pipeline build/test scripts, and pyproject.toml. All this code now lives in
    survey-data-prep repo.

--- src/r/survey/load_data.R
+++
@@ -1,9 +0,0 @@
-load_survey <- function(path, format = c("sav", "csv")) {
-  format <- match.arg(format)
-
-  if (format == "sav") {
-    haven::read_sav(path)
-  } else {
-    readr::read_csv(path, show_col_types = FALSE)
-  }
-}
